05c46a5ec9d5eeb7a993bd788a2c230c5b6c166b
Sam <65307023@qq.com>
2017-08-21 13:43:41 +0800

    change the url

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 define(function () {
     return {
         //ewater服务端url
-        // basicURL: 'http://120.77.246.153:35978'
-        basicURL: 'http://192.168.12.17:22987'
+        basicURL: 'http://120.77.246.153:35978'
+       // basicURL: 'http://192.168.12.17:22987'
     }
 });
