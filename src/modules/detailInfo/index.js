e3687dd7c4ac1c24f8a0f9ac9f079fd66f76fb83
Sherry Chen <1365825528@qq.com>
2017-09-12 18:10:42 +0800

    地图定位

--- src/modules/detailInfo/index.js
+++ src/modules/detailInfo/index.js
@@ -1,18 +1,27 @@
 var template = require('./content.html');
 var eventHelper = require('../../utils/eventHelper');
+//加载地图组件
+var arcgisPlugin = require('modules/arcgisPlugin');
+var mapHelper = require('utils/mapHelper');
 
 // 定义组件
 var comm = Vue.extend({
     template: template,
     data: function () {
         return {
+            setBtn:false,
             dialogImageUrl: '',
-            dialogVisible: false
+            dialogVisible: false,
+            questionTitle:'',
+            isLocated:false
         }
     },
     methods: {
         returnMain:function(){
-            eventHelper.emit('change-menu','new-question');
+            eventHelper.emit('returnBack');
+        },
+        addNewItem:function(){
+            eventHelper.emit('setNormalQues',this.questionTitle);
         },
         handleRemove(file, fileList) {
             console.log(file, fileList);
@@ -20,10 +29,30 @@ var comm = Vue.extend({
         handlePictureCardPreview(file) {
             this.dialogImageUrl = file.url;
             this.dialogVisible = true;
+        },
+        locatePosition:function(){
+            this.isLocated = true;
         }
     },
     mounted: function () {
+        eventHelper.on('openDetailInfo',function(title){
+            if(!!title){
+                this.questionTitle = title;
+                this.setBtn = false;
+                this.isLocated = false;
+            }else{
+                this.isLocated = false;
+                this.questionTitle = '';
+                this.setBtn = true;
+            }
+        }.bind(this));
+        this.map = mapHelper.getArcGISTiledMap('locateMap', 'http://10.194.148.18:6080/arcgis/rest/services/guangzhoumap_gz/MapServer');
+        this.map.on('load', function () {
+            mapHelper.addPoint(this.map, 39366.73260040782, 29446.950962383147, 'img/dirtyPipe.png', {facilityType: 'CP'});
+        }.bind(this));
     },
-    components: {}
+    components: {
+        'arcgis-plugin': arcgisPlugin
+    }
 });
 module.exports = comm;
--- src/modules/newQuestion/index.js
+++ src/modules/newQuestion/index.js
@@ -1,11 +1,14 @@
 var template = require('./content.html');
 var eventHelper = require('../../utils/eventHelper');
+var detailInfo = require('modules/detailInfo');
+
 
 // 定义组件
 var comm = Vue.extend({
     template: template,
     data: function () {
         return {
+            showDetailInfo:false,
             updateNewArr:[
                 {
                     id:1,
@@ -29,11 +32,24 @@ var comm = Vue.extend({
             eventHelper.emit('closeUploadBtn');
         },
         openDetail:function(title){
-            eventHelper.emit('change-menu','detail-info');
+            this.showDetailInfo = true;
+            eventHelper.emit('openDetailInfo',title);
         }
     },
     mounted: function () {
+        eventHelper.on('returnBack',function(){
+            this.showDetailInfo = false;
+        }.bind(this));
+        eventHelper.on('setNormalQues',function(itemTitle){
+            var itemId = this.updateNewArr.length + 1;
+            this.updateNewArr.push({
+                id:itemId,
+                title:itemTitle
+            });
+        }.bind(this));
     },
-    components: {}
+    components: {
+        'detail-info':detailInfo
+    }
 });
 module.exports = comm;
