3608aecc36a42809ceca36f6189cd2ecf1788614
liangbinquan <binquan_liang@163.com>
2017-09-18 13:25:58 +0800

    fix bug

--- src/modules/analyze/index.js
+++ src/modules/analyze/index.js
@@ -27,7 +27,7 @@ var comm = Vue.extend({
             setTimeout(function () {
                 mapHelper.setCenter(44407.60210154524, 28578.197386459644,this.map,8);
             }.bind(this),1500);
-            for(var i=0;i<lineArr.length-1;i++){console.log(1);
+            for(var i=0;i<lineArr.length-1;i++){
                 mapHelper.drawLine(this.map, [lineArr[i].x,lineArr[i].y ], [lineArr[i+1].x,lineArr[i+1].y],5,'#f00');
             }
             mapHelper.addPointAndName(this.map, 44290.52374238852, 28800.580122891788, './img/icon/qidian.png',20,20,true,{facilityType: 'tupian'});
