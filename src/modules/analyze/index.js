1e68248a6f980edba4eb248e8d16e1810a39d6eb
liangbinquan <binquan_liang@163.com>
2017-09-18 17:20:21 +0800

    add charts

--- src/modules/analyze/index.js
+++ src/modules/analyze/index.js
@@ -1,6 +1,7 @@
 var template = require('./content.html');
 var eventHelper = require('../../utils/eventHelper');
 var mapHelper = require('utils/mapHelper');
+var echarts = require('echarts');
 
 // 定义组件
 var comm = Vue.extend({
@@ -13,6 +14,48 @@ var comm = Vue.extend({
     },
     methods: {},
     mounted: function () {
+        var option = {
+            tooltip : {
+                trigger: 'item',
+                formatter: "{a} <br/>{b} : {c} ({d}%)"
+            },
+            // legend: {
+            //     orient: 'vertical',
+            //     left: 'left',
+            //     top:'10%',
+            //     data: ['路面','雨水口','各类检查井','管道、渠箱','边沟','倒虹管','排放口','闸门、阀门、拍门']
+            // },
+            series : [
+                {
+                    name: '访问来源',
+                    type: 'pie',
+                    radius : '50%',
+                    center: ['50%', '50%'],
+                    data:[
+                        {value:650, name:'路面'},
+                        {value:310, name:'雨水口'},
+                        {value:234, name:'各类检查井'},
+                        {value:135, name:'管道、渠箱'},
+                        {value:224, name:'边沟'},
+                        {value:335, name:'倒虹管'},
+                        {value:333, name:'排放口'},
+                        {value:312, name:'闸门、阀门、拍门'}
+                    ],
+                    itemStyle: {
+                        emphasis: {
+                            shadowBlur: 10,
+                            shadowOffsetX: 0,
+                            shadowColor: 'rgba(0, 0, 0, 0.5)'
+                        }
+                    }
+                }
+            ]
+        };
+        var chartsDiv = document.getElementById('xunChaEcharts');
+        var xunChaEcharts = echarts.init(chartsDiv);
+        xunChaEcharts.setOption(option);
+        console.log(123);
+        console.log(echarts);
         var lineArr = [
             {x:44291.429939729016,y:28800.045292982253},
             {x: 44551.780460430055,y:28772.528571282142},
