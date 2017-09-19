99acc7029f8582fd67d396bc7b5f8f48628e2a4f
liangbinquan <binquan_liang@163.com>
2017-09-20 00:59:44 +0800

    modify amap css

--- src/modules/upload/index.js
+++ src/modules/upload/index.js
@@ -48,13 +48,14 @@ var comm = Vue.extend({
             self.locationStatus = '正在定位...';
             AMap.plugin(['AMap.ToolBar', 'AMap.Scale', 'AMap.OverView', 'AMap.Geocoder', 'AMap.Geolocation'], function () {
                 var geolocation = new AMap.Geolocation({
-                    enableHighAccuracy: true,//是否使用高精度定位，默认:true
+                    // enableHighAccuracy: true,//是否使用高精度定位，默认:true
                     timeout: 10000,          //超过10秒后停止定位，默认：无穷大
                     buttonOffset: new AMap.Pixel(10, 20),//定位按钮与设置的停靠位置的偏移量，默认：Pixel(10, 20)
                     zoomToAccuracy: true,      //定位成功后调整地图视野范围使定位位置及精度范围视野内可见，默认：false
-                    buttonPosition: 'LT'
+                    buttonPosition: 'LT',
+                    showButton:false,
                 });
-                self.map.addControl(geolocation);
+                // self.map.addControl(geolocation);
                 geolocation.getCurrentPosition();
                 var geocoder = new AMap.Geocoder({
                     city: "020"//城市，默认：“全国”
